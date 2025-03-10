fbf3e7fe434f4033862051eaab191934494dd50b
GandalphSnaxx <73314864+GandalphSnaxx@users.noreply.github.com>
2025-03-10 17:27:39 -0600

    Updated vscode settings

--- build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/RayRi/Documents/Ray's C++ Libraries/Flag Manager/build/googletest-src/googletest
-# Build directory: C:/Users/RayRi/Documents/Ray's C++ Libraries/Flag Manager/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
